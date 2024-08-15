8cdb89c79515c2948697b621a38e3ec5f030dfa2
Lenovo <goranvujnovic85@gmail.com>
2024-08-15 10:01:02 +0200

    setting up the working environment and first code

---
+++ firstCode.java
@@ -0,0 +1,18 @@
+public class firstCode {
+
+    public static void main(String[] args) {
+
+        int godinaRodjenjaStudenta;
+        int godinaRodjenjaProfesora;
+
+        godinaRodjenjaStudenta = 1988;
+        godinaRodjenjaProfesora = 1957;
+
+        int razlikaUGodinama = godinaRodjenjaStudenta - godinaRodjenjaProfesora;
+
+        String ispis = "Razlika u godinam je: " + razlikaUGodinama + ".";
+
+        System.out.println(ispis);
+
+    }
+}
